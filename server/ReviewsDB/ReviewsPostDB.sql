20f66d43be9c88119c11a7d50b5c7268e58f27a8
Dustin Deitch <DeitchDustin@gmail.com>
2022-06-09 19:42:09 -0700

    Add reviews schema

--- server/ReviewsDB/ReviewsPostDB.sql
+++ server/ReviewsDB/ReviewsPostDB.sql
@@ -1,37 +1,53 @@
+DROP DATABASE IF EXISTS reviews;
+
+CREATE DATABASE reviews;
+
+\connect reviews;
+
 CREATE TABLE reviews(
-    id SERIAL NOT NULL PRIMARY KEY,
-    product_id INT NOT NULL UNIQUE,
-    rating INT NOT NULL,
-    review_date date BIGINT DEFAULT EXTRACT (EPOCH FROM CURRENT_TIMESTAMP),
-    summary VARCHAR(60) NOT NULL,
-    body VARCHAR(1000) NOT NULL,
-    recommend BOOLEAN NOT NULL,
-    reported BOOLEAN DEFAULT false,
-    reviewer_name VARCHAR(60) NOT NULL,
-    reviewer_email VARCHAR(60) NOT NULL UNIQUE,
-    response TEXT,
-    helpfulness NUMERIC DEFAULT 0
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  product_id NUMERIC NOT NULL,
+  rating NUMERIC NOT NULL,
+  date BIGINT NOT NULL,
+  summary TEXT NOT NULL,
+  body TEXT NOT NULL,
+  recommend BOOLEAN NOT NULL,
+  reported BOOLEAN DEFAULT false,
+  reviewer_name TEXT NOT NULL,
+  reviewer_email TEXT NOT NULL,
+  response TEXT,
+  helpfulness NUMERIC DEFAULT 0
 );
 
 CREATE TABLE reviews_images(
-  id SERIAL PRIMARY KEY,
-  review_id INT REFERENCES reviews (id),
-  "url" TEXT
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  review_id NUMERIC NOT NULL,
+  url TEXT
 );
 
 CREATE TABLE characteristics_reviews(
-  id SERIAL PRIMARY KEY,
-  characteristics_id INT NOT NULL,
-  review_id INT REFERENCES reviews (id),
-  "value" INT NOT NULL
-);
-
-CREATE TABLE (
-  
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  characteristics_id NUMERIC NOT NULL,
+  review_id NUMERIC NOT NULL,
+  value NUMERIC NOT NULL
 );
 
 CREATE TABLE characteristics(
-  id INT REFERENCES characteristics_reviews (id),
-  product_id INT REFERENCES reviews (product_id),
-  "name" VARCHAR(8) NOT NULL
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  product_id NUMERIC NOT NULL,
+  name TEXT NOT NULL
 );
+
+COPY reviews (id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) FROM '/Users/deitchdustin/RFP-2204-FEC/server/ReviewsDB/reviews.csv' DELIMITER ',' CSV HEADER;
+COPY reviews_images (id, review_id, "url") FROM '/Users/deitchdustin/RFP-2204-FEC/server/ReviewsDB/reviews_photos.csv' DELIMITER ',' CSV HEADER;
+COPY characteristics_reviews (id, characteristics_id, review_id, "value") FROM '/Users/deitchdustin/RFP-2204-FEC/server/ReviewsDB/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
+COPY characteristics (id, product_id, "name") FROM '/Users/deitchdustin/RFP-2204-FEC/server/ReviewsDB/characteristics.csv' DELIMITER ',' CSV HEADER;
+
+ALTER TABLE reviews ALTER COLUMN date SET DATA TYPE timestamp with time zone USING timestamp with time zone 'epoch' + date * interval '1 millisecond';
+
+ALTER TABLE reviews ALTER COLUMN date SET DEFAULT current_timestamp;
+
+SELECT setval('reviews_id_seq', (SELECT max(id) FROM reviews));
+SELECT setval('reviews_images_id_seq', (SELECT max(id) FROM reviews_images));
+SELECT setval('characteristics_reviews_id_seq', (SELECT max(id) FROM characteristics_reviews));
+SELECT setval('characteristics_id_seq', (SELECT max(id) FROM characteristics));
