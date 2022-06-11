c33ffd7694f2cd89d7b8ed64d867a9bab6cac680
Ryan Snow <74696050+r-snow@users.noreply.github.com>
2022-06-11 12:36:18 -0700

    First controller for product get complete.

--- server/prod_db/prods_controller.js
+++ server/prod_db/prods_controller.js
@@ -8,24 +8,32 @@ module.exports = {
   // Get overview product
   getProductData: (req, res) => {
     let product = {};
+    let features = [];
 
     let params = [
       /*req.body.product.id */
     ];
 
     db.queryAsync(
-      'SELECT JSON_OBJECT('id', id, 'name', name, 'slogan', slogan, 'description',description, 'category', category, 'default_price', default_price) FROM product WHERE product.id = 1;',
-      params
+      'SELECT JSON_OBJECT('id', id, 'name', name, 'slogan', slogan, 'description',description, 'category', category, 'default_price', default_price) FROM product WHERE product.id = 1;'
     )
     .then((results) =>
       product = results;
     )
     .then(() =>
       db.queryAsync(
-        'SELECT JSON_OBJECT (feature, value) FROM features WHERE product_id = 1;'
+        'SELECT JSON_ARRAYAGG(JSON_OBJECT ('feature', feature, 'value', value)) FROM features WHERE product_id = 1;'
       )
+    )
+    .then((results) =>
+      let features = results;
+      product.features = features;
+    )
+    .then((response) =>
+      res.status(200).json(response)
     );
-
-
+    .cath((err) =>
+      res.status(500).json('Something went wrong fetching this product...')
+    )
   },
 };
