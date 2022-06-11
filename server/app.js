a269b09192cda43df651641ef9faea8757087d57
James Reagan <james@jpreagan.com>
2022-06-10 15:39:01 -1000

    Add GET for `/qa/questions`

--- server/app.js
+++ server/app.js
@@ -1,8 +1,24 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const db = require('./db');
 
 const app = express();
 const port = 8080;
+
+app.get('/qa/questions', (req, res, next) => {
+  db.query(
+    'SELECT * FROM questions WHERE product_id = $1',
+    [req.query.product_id],
+    (err, result) => {
+      if (err) {
+        return next(err);
+      }
+      res.send(result.rows);
+      return null;
+    }
+  );
+});
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
