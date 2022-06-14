900176fce16ac23a7413aa1a5ffe168550ab47a0
Dustin Deitch <DeitchDustin@gmail.com>
2022-06-13 17:22:47 -0700

    Need to split routes

--- server/ReviewsDB/ReviewsPostDB.sql
+++ server/ReviewsDB/ReviewsPostDB.sql
@@ -1,3 +1,6 @@
+-- in psql postgres --> \i /Users/deitchdustin/RFP-2204-FEC/server/ReviewsDB/ReviewsPostDB.sql
+--make sure out of reviews db
+
 DROP DATABASE IF EXISTS reviews;
 
 CREATE DATABASE reviews;
@@ -6,41 +9,45 @@ CREATE DATABASE reviews;
 
 CREATE TABLE reviews(
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  product_id NUMERIC NOT NULL,
-  rating NUMERIC NOT NULL,
-  date BIGINT NOT NULL,
-  summary TEXT NOT NULL,
-  body TEXT NOT NULL,
+  product_id INTEGER,
+  rating SMALLINT CHECK (rating > 0 AND rating <= 5),
+  date BIGINT,
+  summary VARCHAR(120),
+  body VARCHAR(1000),
   recommend BOOLEAN NOT NULL,
-  reported BOOLEAN DEFAULT false,
-  reviewer_name TEXT NOT NULL,
-  reviewer_email TEXT NOT NULL,
-  response TEXT,
-  helpfulness NUMERIC DEFAULT 0
+  reported BOOLEAN NOT NULL DEFAULT false,
+  reviewer_name VARCHAR(60),
+  reviewer_email VARCHAR(60),
+  response VARCHAR(1000),
+  helpfulness INTEGER NOT NULL DEFAULT 0
 );
 
-CREATE TABLE reviews_images(
+CREATE TABLE reviews_photos(
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  review_id NUMERIC NOT NULL,
+  review_id INTEGER NOT NULL  REFERENCES reviews ON DELETE CASCADE,
   url TEXT
-);
-
-CREATE TABLE characteristics_reviews(
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  characteristics_id NUMERIC NOT NULL,
-  review_id NUMERIC NOT NULL,
-  value NUMERIC NOT NULL
 );
 
 CREATE TABLE characteristics(
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  product_id NUMERIC NOT NULL,
-  name TEXT NOT NULL
+  product_id INTEGER NOT NULL,
+  name TEXT
+);
+
+
+CREATE TABLE characteristics_reviews(
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  characteristic_id INTEGER NOT NULL,
+  review_id INTEGER NOT NULL REFERENCES reviews ON DELETE CASCADE,
+  value SMALLINT CHECK (vaLue > 0 AND value <= 5)
 );
 
 COPY reviews (id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) FROM '/Users/deitchdustin/RFP-2204-FEC/server/ReviewsDB/reviews.csv' DELIMITER ',' CSV HEADER;
-COPY reviews_images (id, review_id, "url") FROM '/Users/deitchdustin/RFP-2204-FEC/server/ReviewsDB/reviews_photos.csv' DELIMITER ',' CSV HEADER;
-COPY characteristics_reviews (id, characteristics_id, review_id, "value") FROM '/Users/deitchdustin/RFP-2204-FEC/server/ReviewsDB/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
+
+COPY reviews_photos (id, review_id, "url") FROM '/Users/deitchdustin/RFP-2204-FEC/server/ReviewsDB/reviews_photos.csv' DELIMITER ',' CSV HEADER;
+
+COPY characteristics_reviews (id, characteristic_id, review_id, "value") FROM '/Users/deitchdustin/RFP-2204-FEC/server/ReviewsDB/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
+
 COPY characteristics (id, product_id, "name") FROM '/Users/deitchdustin/RFP-2204-FEC/server/ReviewsDB/characteristics.csv' DELIMITER ',' CSV HEADER;
 
 ALTER TABLE reviews ALTER COLUMN date SET DATA TYPE timestamp with time zone USING timestamp with time zone 'epoch' + date * interval '1 millisecond';
@@ -48,6 +55,13 @@ ALTER TABLE reviews ALTER COLUMN date SET DATA TYPE timestamp with time zone USI
 ALTER TABLE reviews ALTER COLUMN date SET DEFAULT current_timestamp;
 
 SELECT setval('reviews_id_seq', (SELECT max(id) FROM reviews));
-SELECT setval('reviews_images_id_seq', (SELECT max(id) FROM reviews_images));
+SELECT setval('reviews_photos_id_seq', (SELECT max(id) FROM reviews_photos));
 SELECT setval('characteristics_reviews_id_seq', (SELECT max(id) FROM characteristics_reviews));
 SELECT setval('characteristics_id_seq', (SELECT max(id) FROM characteristics));
+
+CREATE INDEX review_product_id_index ON reviews (product_id);
+CREATE INDEX characteristics_characteristic_id_index ON characteristics_reviews (characteristic_id);
+CREATE INDEX characteristics_index ON characteristics (id);
+CREATE INDEX review_photos_review_id ON reviews_photos (review_id);
+
+
--- server/app.js
+++ server/app.js
@@ -1,8 +1,97 @@
 const express = require('express');
 const path = require('path');
 
+const db = require('./db');
+
 const app = express();
 const port = 8080;
+
+
+// GET Requests
+app.get('/reviews', (req, res, next) => {
+  console.log(req.query.review_id);
+  db.query('SELECT * FROM reviews WHERE product_id = $1 AND reported = false', [req.query.product_id], (err, result) => {
+    if(err) {
+      return next(err);
+    }
+    res.send(result.rows); // Docs show result.rows[0]
+    return null;
+  })
+});
+
+
+app.get('/reviews_photos', (req, res, next) => {
+  console.log(req.query.review_id);
+  db.query('SELECT "url" FROM reviews_photos WHERE review_id = $1', [req.query.review_id], (err, result) => {
+    if(err) {
+      return next(err);
+    }
+    res.send(result.rows);
+    return null
+  })
+})
+
+app.get('') // How to get characteristics?
+
+// POST requests
+app.post('/characteristics_reviews', (req, res, next) => {
+  console.log(req.query)
+  db.query(
+    'INSERT INTO characteristics_reviews (characteristic_id, review_id, value) VALUES ($1, $2, $3)', [req.query.characteristic_id, req.query.review_id, req.query.value], (err, result) => {
+      if (err) {
+        console.log(err, 'NOOO')
+        return next(err);
+      }
+      res.status(201).send(result.rows);
+      return null;
+    })
+})
+
+app.post('/reviews', (req, res, next) => {
+  console.log(req.query.product_id);
+  db.query(`INSERT INTO reviews
+  (product_id, rating, "date", summary, body, recommend, reviewer_name, reviewer_email)
+  VALUES
+  ($1, $2, $3, $4, $5, $6, $7, $8)`,
+  [
+    req.query.product_id,
+    req.query.rating,
+    new Date().toISOString(),
+    req.query.summary,
+    req.query.body,
+    req.query.recommend,
+    req.query.name,
+    req.query.email
+  ],
+  (err, result) => {
+    if(err) {
+      return next(err);
+    }
+    res.status(200).send(result.rows); // Docs show result.rows[0]
+    return null;
+  })
+});
+
+// PUT requests
+
+
+
+
+// reviewAdd: (req, res) => {
+
+// },
+
+// reportPut: (req, res) => {
+
+// },
+
+// helpfulPut: (req, res) => {
+
+// },
+
+// characteristicsGet: (req, res) => {
+
+// }
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
