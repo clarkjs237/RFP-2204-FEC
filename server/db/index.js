2c627f7940e9a3d19f40af0d93537d9f1a829f5e
James Reagan <james@jpreagan.com>
2022-06-16 16:17:15 -1000

    Refer to host in .env

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.PGUSER,
-  host: 'localhost',
+  host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: 5432,
